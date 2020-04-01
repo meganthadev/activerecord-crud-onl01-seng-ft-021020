b1d047978f487f63f222d0333d63e3344f87b7a4
Megan Horsfield <meganthadev@gmail.com>
2020-04-01 06:16:42 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,10 +29,10 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   # release_date == 1990
   
   Movie.create do |m|
-    m.title = "Home Alone"
-    m.release_date = 1990
+    m.title = args[:title]
+    m.release_date = args[:release_date]
   end
-end
+
 
 def can_get_the_first_item_in_the_database
   Movie.first.title
