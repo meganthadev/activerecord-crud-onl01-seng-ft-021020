7efb1377f6f3647dba3083b83d1bb5f3ba383423
Megan Horsfield <meganthadev@gmail.com>
2020-04-01 05:59:28 +0000

    Automatically backed up by Learn

--- db/migrate/20200401055725_create_movies.rb
+++ db/migrate/20200401055725_create_movies.rb
@@ -4,7 +4,9 @@ class CreateMovies < ActiveRecord::Migration[5.2]
     create_table :movies do |t|
       t.string :title 
       t.integer :release_date
-      t.string
+      t.string :director
+      t.string :director
+
   end
   
 end
